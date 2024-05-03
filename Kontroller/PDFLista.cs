7e22918a00a2ea4ba7ed05c3ca03734c1c6e860e
Göran Härdelin <goran.hardelin@outlook.com>
2024-05-03 14:54:58 +0100

    Ändrat iTextSharp, som nu är deprecated, till iText7. Då funkade det naturligtvis inte. Mycket kod är då utkommenterad.

--- Kontroller/PDFLista.cs
+++ Kontroller/PDFLista.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 using Valvetwebb.Aktivitet;
 using Valvetwebb.Objekt;
 using System.IO;
 using System.Drawing;
+using iText.Layout;
 
 namespace Valvetwebb.Kontroller
 {
@@ -62,70 +64,56 @@ namespace Valvetwebb.Kontroller
 
         public static void CreatePdf()
         {
-            List<ValvPost> valvpostList = GetData();
+            //List<ValvPost> valvpostList = GetData();
 
-            Document document = new Document();
-            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(@"C:\\Mina program\\Valvet\\" + "ValvetLista.pdf", FileMode.Create));
-            DataTable dataTable = GenereraDataTable(valvpostList);
+            //Document document = new Document();
+            //PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(@"C:\\Mina program\\Valvet\\" + "ValvetLista.pdf", FileMode.Create));
+            //DataTable dataTable = GenereraDataTable(valvpostList);
 
-            var header = new PDFFooter();
-            document.Open();
-            writer.PageEvent = header;
-            header.HeaderText = "Valvlista";
-            iTextSharp.text.Font fontH = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 7, 2);
-            iTextSharp.text.Font fontP = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 6);
-            iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
+            //var header = new PDFFooter();
+            //document.Open();
+            //writer.PageEvent = header;
+            //header.HeaderText = "Valvlista";
+            //iTextSharp.text.Font fontH = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 7, 2);
+            //iTextSharp.text.Font fontP = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 6);
+            //iTextSharp.text.Font font5 = iTextSharp.text.FontFactory.GetFont(FontFactory.HELVETICA, 5);
 
-            fontH.Color = BaseColor.GRAY;
-            //Chunk chkHeader = new Chunk("ValvetLista", fontH);
-            //Phrase p1 = new Phrase(chkHeader);
+            //fontH.Color = BaseColor.GRAY;
 
-            //Header objHeaderFooter = new Header();
-            //objHeaderFooter.setHeader(new Phrase(HeaderText));
-            //writer.PageEvent = new pdf objHeaderFooter;
+            //PdfPTable table = new PdfPTable(dataTable.Columns.Count);
+            //table.HeaderRows = 1; /*---->> this property repeats the headers of an iTextSharp PdfPTable on each page */
+            //PdfPRow row = null;
+            //float[] widths = new float[dataTable.Columns.Count];
+            //for (int i = 0; i < dataTable.Columns.Count; i++)
+            //    widths[i] = 4f;
 
-            //document.= new HeaderFooter(p1, false);
+            //table.SetWidths(widths);
+            //table.WidthPercentage = 100;
+            //int iCol = 0;
+            //string colname = "";
+            //PdfPCell cell = new PdfPCell(new Phrase("Valvposter"));
 
-            //header.Alignment = 1;
+            //cell.Colspan = dataTable.Columns.Count;
 
-            PdfPTable table = new PdfPTable(dataTable.Columns.Count);
-            table.HeaderRows = 1; /*---->> this property repeats the headers of an iTextSharp PdfPTable on each page */
-            //table.TotalWidth = 900f;
-            //table.LockedWidth = true;
-            PdfPRow row = null;
-            float[] widths = new float[dataTable.Columns.Count];
-            for (int i = 0; i < dataTable.Columns.Count; i++)
-                widths[i] = 4f;
-            //float[] widths = new float[] { 4f, 4f, 4f, 4f };
+            //foreach (DataColumn c in dataTable.Columns)
+            //{
 
-            table.SetWidths(widths);
-            table.WidthPercentage = 100;
-            int iCol = 0;
-            string colname = "";
-            PdfPCell cell = new PdfPCell(new Phrase("Valvposter"));
+            //    table.AddCell(new Phrase(c.ColumnName, fontH));
+            //}
 
-            cell.Colspan = dataTable.Columns.Count;
-            //cell.Colspan = 5;
+            //if (valvpostList.Count > 0)
+            //{
+            //    foreach (ValvPost valvpost in valvpostList)
+            //    {
+            //        table.AddCell(new Phrase(valvpost.Postnamn.ToString(), fontP));
+            //        table.AddCell(new Phrase(valvpost.Usernamn.ToString(), font5));
+            //        table.AddCell(new Phrase(valvpost.Losenord.ToString(), font5));
+            //        table.AddCell(new Phrase(valvpost.Anteckningar.ToString(), font5));
+            //    }
+            //}
 
-            foreach (DataColumn c in dataTable.Columns)
-            {
-
-                table.AddCell(new Phrase(c.ColumnName, fontH));
-            }
-
-            if (valvpostList.Count > 0)
-            {
-                foreach (ValvPost valvpost in valvpostList)
-                {
-                    table.AddCell(new Phrase(valvpost.Postnamn.ToString(), fontP));
-                    table.AddCell(new Phrase(valvpost.Usernamn.ToString(), font5));
-                    table.AddCell(new Phrase(valvpost.Losenord.ToString(), font5));
-                    table.AddCell(new Phrase(valvpost.Anteckningar.ToString(), font5));
-                }
-            }
-
-            document.Add(table);
-            document.Close();
+            //document.Add(table);
+            //document.Close();
         }
 
         private static DataTable GenereraDataTable(List<ValvPost> dt)
